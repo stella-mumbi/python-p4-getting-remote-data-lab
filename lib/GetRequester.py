beb4330a90349a7b8c16d636db680279ca93c512
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-04-08 14:54:04 +0300

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,29 @@
 import requests
 import json
 
+
 class GetRequester:
 
+    # initialize method + instance var to accept any URL
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
         pass
+        URL = self.url
+
+        response = requests.get(URL)
+
+        return response.content
 
     def load_json(self):
         pass
+        json_content = json.loads(self.get_response_body())
+        # data = json.dumps(json_content, indent=4, sort_keys=True)
+
+        return json_content
+
+
+get_requester = GetRequester(
+    'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
